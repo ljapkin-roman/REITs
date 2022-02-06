17de0448d2d52b1114ff603064fce09ada713161
Roma <silvervaza@gmail.com>
2022-02-06 12:22:08 +0100

    create directory for mvc model before install eloquent

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+require "../vendor/autoload.php";
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+// Run app
+$app->run();
